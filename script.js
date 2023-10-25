83d266e6136edebe8be2a6f96d24ce7ad3e6f24d
Xondamir <abdulazizovxondamirxon@gmail.com>
2023-10-25 07:48:58 +0500

    tugallandi

---
+++ script.js
@@ -0,0 +1,88 @@
+// let a = "1234.56";
+// Number() constructor function
+// console.log (parseInt(a));
+// console.log(parseFloat(a));
+// console.log(+a)
+// console.log(-a)
+// console.log( a * 1)
+// console.log( a / 1)
+// console.log( a - 1)
+// console.log(a + 1);
+// console.log (3 + a)
+// number , beolean => string
+// let a = 1011;
+// String () constructor function
+// console.log(String(a));
+// console.log(a + "")
+
+// Number => String
+// console.log(a.toString(a));
+// console.log(a.toFixed(1))
+
+// Operators
+// Aritmetic
+// +, -, *, /, %, ++, --, **
+
+// console.log(10 % 3);
+// let a = 1;
+// console.log(a)
+// console.log(++a);
+// console.log(--a);
+// console.log(2 ** 4)
+
+// Comparison (Taqqoslash)
+// >, <a, <=, >=, ==, ===, !=, !==
+
+// console.log(1 > 2);
+// console.log(1 > 2);
+// console.log(1 < 1);
+// console.log(1  <= 1 );
+// console.log(1 >= 1);
+// console.log(1 > 1);
+// console.log(1 == 2);
+// console.log(1 == '1');
+// console.log(1 === '1');
+// console.log(1 != "1");
+// console.log(1 !== "1");
+
+// Logical Operators
+// and => &&, or || , not !
+// not
+// console.log(!true)
+// console.log(!false)
+
+// or
+// console.log(true || false) => true
+// console.log(false || false) => false
+
+// and
+
+// console.log(true && true);
+// console.log(true && false);
+// console.log(false && true);
+// console.log(false && false);
+
+// to Boolean
+// Boolean() Constructor Function
+// Falsy => 0, -0, NaN , '', "", null, undefined, false
+// Truthy => others, " ", 'a'
+// let a = ;
+
+// console.log(Boolean(a))
+// console.log(!!a)
+
+// person.email = "eshmat@gmail.com";
+// person["phone"] = "+998901301850";
+// console.log(person);
+
+let yosh = prompt("Yoshingizni kiriting: ");
+console.log("Siz " + (2023 - yosh) + "- yilda tug'ilgansiz.");
+console.log(
+  "3 yildan keyin siz " + (3 + +yosh) + " yoshga kirgan bo'lasiz"
+);
+let old = prompt("Siz Nechchi yoshda bo'lib qolishni xohlaysiz: ");
+console.log(
+  "Unda siz yana " + (old - yosh) + " yil kutishingiz kerak."
+);
+
+
